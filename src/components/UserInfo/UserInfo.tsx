6602093aa649ffa4a0f80143458a476262e57408
chenxin176 <chenxin176@meituan.com>
2026-04-28 17:06:07 +0800

    chore: 清理 master 遗留文件(src/, public/, coverage/ 等), .gitignore 屏蔽 AI 工具配置

--- src/components/UserInfo/UserInfo.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { LOGIN } from '../../router'
-
-export default function UserInfo() {
-  return <Link to={LOGIN}>登录</Link>
-}
--- src/constants/CanvasRendering.ts
+++
@@ -1,7 +0,0 @@
-//缩放块的实际尺寸和相应尺寸
-export const RESIZERECTSIZE = 7
-
-//节点盒的内边距
-export const BOXPADDING = 10
-//节点盒的外边距
-export const BOXMARGIN = 5
