b0bd74ef9b5802ed69f288bd8465d6b8148a7cf0
chenxin176 <chenxin176@meituan.com>
2026-04-28 16:59:49 +0800

    chore: 删除冗余 desktop/ 目录, 将 lunlunglass-backend 加入 dev 脚本

--- desktop/router/statics.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router')
-const StaticsController = require('../controllers/Statics')
-const router = new Router({
-  prefix: '/api/statics',
-})
-
-router.get('/', StaticsController.getStatics)
-
-module.exports = router
