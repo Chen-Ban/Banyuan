532392b062c42c2b6a4f511be801cf2ad255b3dc
chenxin176 <chenxin176@meituan.com>
2026-04-28 16:59:49 +0800

    chore: 删除冗余 desktop/ 目录, 将 lunlunglass-backend 加入 dev 脚本

--- desktop/services/Statics.js
+++
@@ -1,9 +0,0 @@
-const Statics = require('../database/models/Statics')
-
-class StaticsServices {
-  async getStatics() {
-    return await Statics.find({})
-  }
-}
-
-module.exports = new StaticsServices()
