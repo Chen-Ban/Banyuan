99c9046db9ed307bc2bd06f78ae31755e47ae429
chenxin176 <chenxin176@meituan.com>
2026-05-12 17:25:42 +0800

    refactor(hook): split useCanvasInit into useDesignCanvasInit and useRuntimeCanvasInit

--- packages/BanvasGL/src/hook/index.ts
+++ packages/BanvasGL/src/hook/index.ts
@@ -1,3 +1,7 @@
 export { default as useDesignBanvas } from "./useDesignBanvas";
 export { default as useRuntimeBanvas } from "./useRuntimeBanvas";
 export { default as useFlowBanvas } from "./useFlowBanvas";
+export { useDesignCanvasInit } from "./useDesignCanvasInit";
+export type { UseDesignCanvasOptions, UseDesignCanvasInitResult } from "./useDesignCanvasInit";
+export { useRuntimeCanvasInit } from "./useRuntimeCanvasInit";
+export type { UseRuntimeCanvasOptions, UseRuntimeCanvasInitResult } from "./useRuntimeCanvasInit";
