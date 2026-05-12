ce4dcad7f8501b8a973bcb0fe160606d689061a3
chenxin176 <chenxin176@meituan.com>
2026-05-12 16:11:27 +0800

    chore(hook): remove backward-compat re-export shims in useDesignBanvas/canvas

--- packages/BanvasGL/src/hook/useDesignBanvas/canvas/useCanvasInit.ts
+++
@@ -1,3 +0,0 @@
-// 已迁移到 hook/useCanvasInit/index.ts
-// 此文件保留为向后兼容的重新导出
-export { useCanvasInit, type UseCanvasInitResult, type UseCanvasOptions, type SerializedPageJSON } from '../../useCanvasInit'
--- packages/BanvasGL/src/hook/useDesignBanvas/canvas/useRuntimeEvents.ts
+++
@@ -1,3 +0,0 @@
-// 已迁移到 hook/useRuntimeBanvas/canvas/useRuntimeEvents.ts
-// 此文件保留为向后兼容的重新导出
-export { useRuntimeEvents, type UseRuntimeEventsOptions } from '../../useRuntimeBanvas/canvas/useRuntimeEvents'
